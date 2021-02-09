be41451b80cc264abf996b4f2028ef6cf02746aa
Jack Schluger <jes543@cornell.edu>
2021-02-08 23:58:11 -0500

    adding javascript file

---
+++ java.js
@@ -0,0 +1,18 @@
+var state = 0;
+var swap = function() {
+    if (state == 0) {
+	$("#academic-intro").hide();
+	$("#personal-intro").show();
+
+	state = 1;
+    }
+    else {
+	$("#academic-intro").show();
+	$("#personal-intro").hide();
+
+	state = 0;
+    } 
+};
+
+swap();
+$("#rotate").click(swap);
