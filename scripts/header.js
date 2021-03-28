fa3fd795aefb8de851cdc40f1bc200d3631e3a3f
Jack Schluger <jes543@cornell.edu>
2021-03-28 15:18:44 -0400

    arrows

--- scripts/header.js
+++ scripts/header.js
@@ -1,30 +1,38 @@
 var red_f = 1.5/12;
-var blue_f = 3/12;
+var blue_f = 5/12;
 
 var RED = "#a05d68";
 var BLUE = "#282d44";
+var BLACK = "black";
 
+var NAME = "Jack Schluger";
+var COOKIE_NAME = "header";
 
-
-var decorate_name = function(name) {
-    var name = $('#name').text();
+function decorate_name(name, colors) {
     var out = "";
     for (var i = 0; i < name.length; i++) {
-	var letter = name[i];
+	out += '<span style="color:'+ colors[i] +'">'+name[i]+'</span>';
+    }
+    return out;
+}
+
+function pick_colors(name) {
+    var colors = [];
+    for (var i = 0; i < name.length; i++) {
 	if (Math.random() < red_f) {
-	    out += '<span style="color:'+ RED +'">'+letter+'</span>';
+	    colors.push(RED);
 	}
 	else {
 	    var r =  Math.random();
 	    if (red_f < r && r < red_f + blue_f) {
-		out += '<span style="color:'+ BLUE +'">'+letter+'</span>';
+		colors.push(BLUE);
 	    }
 	    else {
-		out += letter;
+		colors.push(BLACK);
 	    }
 	}
     }
-    return out;
+    return colors;
 }
 
 // https://coderrocketfuel.com/article/how-to-create-read-update-and-delete-cookies-in-javascript
@@ -44,11 +52,10 @@ function getCookieValue(name) {
 
 
 function main() {
-    var cookie_name = "header"
     var MAX_C = 2;
     console.log('unparsed cookie is');
     console.log(document.cookie);
-    var val = getCookieValue(cookie_name);
+    var val = getCookieValue(COOKIE_NAME);
     console.log('val: <'+val+'>');
     console.log(typeof(val));
     var data = {};
@@ -66,22 +73,46 @@ function main() {
     } else { console.log('setting data'); }
     console.log(data);
     
-    if ((!('c' in data)) || data.c >= MAX_C) {
+    if ((!('c' in data && 'colors' in data)) || data.c >= MAX_C) {
 	console.log(!('c' in data));
 	console.log( data.c == MAX_C);
 	console.log('case 1');
         data.c = 0;
-        data.html = decorate_name("Jack Schluger");
-        $('#name').html(data.html);
+        data.colors = pick_colors(NAME);
+        $('#name').html(decorate_name(NAME, data.colors));
     }
     else {
 	console.log('case 2');
         data.c += 1;
-        $('#name').html(data.html);
+	
+        $('#name').html(decorate_name(NAME, data.colors));
     }
     console.log('writing data');
     console.log(data);
-    document.cookie = cookie_name+"="+JSON.stringify(data)
+    document.cookie = COOKIE_NAME+"="+JSON.stringify(data)
 }
 
 main();
+
+function rot_right(lst) {
+    lst.unshift(lst.pop());
+}
+function rot_left(lst) {
+    lst.push(lst.shift());
+}
+
+function rotate_colors_right() {
+    var val = getCookieValue(COOKIE_NAME);
+    var data = JSON.parse(val);
+    rot_right(data.colors);
+    document.cookie = COOKIE_NAME+"="+JSON.stringify(data)
+    $('#name').html(decorate_name(NAME, data.colors));
+}
+
+function rotate_colors_left() {
+    var val = getCookieValue(COOKIE_NAME);
+    var data = JSON.parse(val);
+    rot_left(data.colors);
+    document.cookie = COOKIE_NAME+"="+JSON.stringify(data)
+    $('#name').html(decorate_name(NAME, data.colors));
+}
