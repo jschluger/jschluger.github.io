928c4d4ed5a44e5c5c70410640d604b43de15c4c
Charlotte <charlotteschluger@gmail.com>
2026-06-09 09:17:53 -0400

    margin bottom

--- scripts/photos.js
+++ scripts/photos.js
@@ -1,4 +1,5 @@
 $('body').css("background", "black");
-$('#content').css("background", "black");
+$('#content-overlay').css("background", "black");
 $('body').css("color", "white");
 $('.8131').css('z-index','0')
+$('#pwd').css('color','white')
